b979ebe060303e5e18a6f4870a7fed6fc61adc30
Alex Bombita <abombita707@gmail.com>
2022-06-03 13:42:54 -0700

    algos update

---
+++ algos/month_1/day_1.js
@@ -0,0 +1,10 @@
+// VARIABLE allow us to hold and manipulate data
+
+var a = "Edward Im"
+var myName = "Ed"
+var other_full_name = "Jane Doe"
+
+//1. Declare a variable
+var age 
+//2. Initializing a variable
+age = 11 
